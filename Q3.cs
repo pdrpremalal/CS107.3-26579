f0ccc547ae42a49e9352aa3b8e90d1c19e48dc6f
pdrpremalal <138178712+pdrpremalal@users.noreply.github.com>
2023-07-10 22:38:47 -0700

    Add files via upload

---
+++ Q3.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter km value:");
+            double km = double.Parse(Console.ReadLine());
+
+            ConvertValues obj = new ConvertValues();
+            double m = obj.kilometerTOmeter(km);
+
+            Console.Write(m.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
