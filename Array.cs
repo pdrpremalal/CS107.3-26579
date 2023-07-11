b7bd91f34d8a71d820f3d89bb6b6a5e1aa11709f
pdrpremalal <138178712+pdrpremalal@users.noreply.github.com>
2023-07-10 22:34:41 -0700

    Add files via upload

---
+++ Array.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q_1._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter your batch: ");
+            string batch = Console.ReadLine();
+
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Batch: " + batch);
+
+            Console.ReadLine();
+        }
+    }
+}
