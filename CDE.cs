8fbe5632e5991109a15df5f72fb40d42a83f9f31
pdrpremalal <138178712+pdrpremalal@users.noreply.github.com>
2023-07-11 07:57:29 -0700

    Add files via upload

---
+++ CDE.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            ArithmeticOperations operations = new ArithmeticOperations();
+
+            Console.Write("Enter number 1:");
+            double number1 = double.Parse(Console.ReadLine());
+            operations.Number1 = number1;
+
+            Console.Write("Enter number 2:");
+            double number2 = double.Parse(Console.ReadLine());
+            operations.Number2 = number2;
+
+            Console.WriteLine("Summation:" + operations.Summation());
+            Console.WriteLine("Subtraction:" + operations.Subtraction());
+            Console.WriteLine("Multiplication:" + operations.Multiplication());
+
+            try
+            {
+                Console.WriteLine("Division:" + operations.Division());
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
