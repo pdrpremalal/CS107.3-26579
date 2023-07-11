ab236145f79b0583a37b1125c86bfceaa1fe2f38
pdrpremalal <138178712+pdrpremalal@users.noreply.github.com>
2023-07-10 22:37:23 -0700

    Add files via upload

---
+++ Hello.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 c1 = new Class1();
+            c1.sayhello2();
+            Console.ReadLine();
+            Console.ReadLine();
+        }
+    }
+}
