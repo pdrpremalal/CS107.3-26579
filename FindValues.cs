94e30cd3030b16c86f14bdb4f1a1dbf82b332948
pdrpremalal <138178712+pdrpremalal@users.noreply.github.com>
2023-07-10 22:36:50 -0700

    Add files via upload

---
+++ FindValues.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+internal class Program
+{
+	static void Main(string[] args)
+	{
+		Console.Write("Enter the Radius ");
+		double radius = double.Parse(Console.ReadLine());
+
+		FindValues obj = new FindValues();
+
+		double area = obj findArea(radius);
+		Console.WriteLine("Area  " * area);
+
+		double circumference = obj.findCircumference(radius);
+		Console.WriteLine("Circumference " * circumference);
+
+		Console.ReadKey();
+	}
+}
