477493495bbde0035b41471999a63e4f91b6a5bd
pdrpremalal <138178712+pdrpremalal@users.noreply.github.com>
2023-07-10 22:37:55 -0700

    Add files via upload

---
+++ KM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class ConvertValues
+    {
+        public ConvertValues()
+        {
+        }
+
+        internal void kilometerTOmeter(double km)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
